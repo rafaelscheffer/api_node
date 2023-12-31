ccce9d10f57277915b09f8e2a38811ddb396e61f
Rafael Scheffer <rafaelscheffer@estrela10.com.br>
2023-12-31 16:44:31 -0300

    ajustes e correções

--- src/controllers/get-users/get-users.ts
+++ src/controllers/get-users/get-users.ts
@@ -1,21 +1,18 @@
-import { IGetUsersController, IGetUsersRepository } from "./protocols";
+import { User } from "../../models/user";
+import { ok, serverError } from "../helpers";
+import { HttpResponse, IController } from "../protocols";
+import { IGetUsersRepository } from "./protocols";
 
-export class GetUsersController implements IGetUsersController {
-    constructor(private readonly getUsersRepository: IGetUsersRepository){}
+export class GetUsersController implements IController {
+  constructor(private readonly getUsersRepository: IGetUsersRepository) {}
 
-    async handle() {
-        try {
-          const users = await this.getUsersRepository.getUsers();
+  async handle(): Promise<HttpResponse<User[] | string>> {
+    try {
+      const users = await this.getUsersRepository.getUsers();
 
-          return {
-            statusCode: 200,
-            body: users
-          }
-        } catch (error){
-          return {
-            statusCode: 500,
-            body: "Algo deu errado."
-          }
-        }
+      return ok<User[]>(users);
+    } catch (error) {
+      return serverError();
     }
+  }
 }
--- src/repositories/get-users/mongo-get-users.ts
+++ src/repositories/get-users/mongo-get-users.ts
@@ -4,15 +4,15 @@ import { User } from "../../models/user";
 import { MongoUser } from "../mongo-protocols";
 
 export class MongoGetUsersRepository implements IGetUsersRepository {
-    async getUsers(): Promise<User[]> {
-      const users = await MongoClient.db
-        .collection<MongoUser>("users")
-        .find({})
-        .toArray();
-  
-      return users.map(({ _id, ...rest }) => ({
-        ...rest,
-        id: _id.toHexString(),
-      }));
-    }
+  async getUsers(): Promise<User[]> {
+    const users = await MongoClient.db
+      .collection<MongoUser>("users")
+      .find({})
+      .toArray();
+
+    return users.map(({ _id, ...rest }) => ({
+      ...rest,
+      id: _id.toHexString(),
+    }));
+  }
 }
