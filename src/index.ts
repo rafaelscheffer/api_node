5a299a1ac27954819bd474ac91dadc8079c00c59
Rafael Scheffer <rafaelscheffer@estrela10.com.br>
2023-12-31 11:14:45 -0300

    listando usuários do banco de dados mongo

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,14 @@ import express from "express";
 import { config } from "dotenv";
 import { GetUsersController } from "./controllers/get-users/get-users";
 import { MongoGetUsersRepository } from "./repositories/get-users/mongo-get-users";
+import { MongoClient } from "./database/mongo";
 
 const main = async () => {
   config();
   const app = express();
+
+  await MongoClient.connect();
+
   const port = process.env.PORT || 9000;
 
   app.get("/users", async (req, res) => {
